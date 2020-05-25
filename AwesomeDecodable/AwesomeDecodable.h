d4f5942c2962a4d5dc9f60609fa3d8a9c0b16104
Thibaut Richez <thibaut.richez@benextcompany.com>
2020-05-25 14:20:14 +0200

    Initial Commit

---
+++ AwesomeDecodable/AwesomeDecodable.h
@@ -0,0 +1,19 @@
+//
+//  AwesomeDecodable.h
+//  AwesomeDecodable
+//
+//  Created by RICHEZ Thibaut on 5/25/20.
+//  Copyright © 2020 richez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AwesomeDecodable.
+FOUNDATION_EXPORT double AwesomeDecodableVersionNumber;
+
+//! Project version string for AwesomeDecodable.
+FOUNDATION_EXPORT const unsigned char AwesomeDecodableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AwesomeDecodable/PublicHeader.h>
+
+
---
+++ AwesomeDecodableTests/AwesomeDecodableTests.swift
@@ -0,0 +1,34 @@
+//
+//  AwesomeDecodableTests.swift
+//  AwesomeDecodableTests
+//
+//  Created by RICHEZ Thibaut on 5/25/20.
+//  Copyright © 2020 richez. All rights reserved.
+//
+
+import XCTest
+@testable import AwesomeDecodable
+
+class AwesomeDecodableTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
